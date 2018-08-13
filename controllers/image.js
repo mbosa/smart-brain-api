952fd07dd5a047ffb37224d0f26ddcd3c67c1057
n4bb0 <goal.bandicoot@gmail.com>
2018-08-13 16:06:14 +0200

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '6f9a819156244d8e8cb9c0956bd6d13c'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleApiCall: handleApiCall,
+  handleImage: handleImage
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'marco',
+    password : 'ravelivet0',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+/* END POINTS
+/ (root) --> GET
+/signin --> POST (POST, not GET, because it's sending a password, and POST conceals it)
+/register --> POST
+/profile/:userId --> GET
+/image --> PUT
+*/
+
+// / (root)
+app.get('/', (req, res)=> { res.status(200).json('success') })
+// /signin
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+// /register --> add a new user
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+// /profile/:id  (:id means that it accepts anything and it can be grabbed as req.params.id)
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+// /image --> update number of entries
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}...`);
+});
+
+// app.listen(3000, ()=> {
+//   console.log('app is running on port 3000');
+// })
